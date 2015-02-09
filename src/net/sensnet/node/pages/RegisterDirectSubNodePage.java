6b49bb3fee791743c240d8a085ffe94d5012f381
Janis Streib <janis@dogcraft.de>
2015-02-09 23:29:28 +0100

    ADD: Form

--- src/net/sensnet/node/pages/RegisterDirectSubNodePage.java
+++ src/net/sensnet/node/pages/RegisterDirectSubNodePage.java
@@ -1,10 +1,12 @@
 package net.sensnet.node.pages;
 
 import java.io.IOException;
+import java.util.HashMap;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import net.sensnet.node.Form;
 import net.sensnet.node.Page;
 
 public class RegisterDirectSubNodePage extends Page {
@@ -16,6 +18,8 @@ public class RegisterDirectSubNodePage extends Page {
 	@Override
 	public void doGet(HttpServletRequest req, HttpServletResponse resp)
 			throws IOException {
+		Form f = new RegisterDirectSubNodeForm(req);
+		f.output(resp.getWriter(), new HashMap<String, Object>());
 
 	}
 
