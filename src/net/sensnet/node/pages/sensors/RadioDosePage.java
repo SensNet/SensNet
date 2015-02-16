6a1123f9b10d0a2be634431d1967b3a7b91d56b1
Janis Streib <janis@dogcraft.de>
2015-02-16 18:20:54 +0100

    add: show nodes overview on sensors page

--- src/net/sensnet/node/pages/sensors/RadioDosePage.java
+++ src/net/sensnet/node/pages/sensors/RadioDosePage.java
@@ -1,6 +1,8 @@
 package net.sensnet.node.pages.sensors;
 
 import java.io.IOException;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.Date;
 import java.util.Map;
@@ -8,7 +10,9 @@ import java.util.Map;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import net.sensnet.node.DatabaseConnection;
 import net.sensnet.node.Page;
+import net.sensnet.node.SensNetNodeConfiguration;
 import net.sensnet.node.dbobjects.sensors.RadioDoseSensor;
 import net.sensnet.node.pages.NodesOverviewPage;
 
@@ -28,6 +32,91 @@ public class RadioDosePage extends Page {
 			final int[][] data = RadioDoseSensor.getDoses(
 					new Date(System.currentTimeMillis() - 1000 * 60 * 60 * 24),
 					new Date(System.currentTimeMillis()));
+			PreparedStatement nodes = DatabaseConnection.getInstance().prepare(
+					"SELECT * FROM nodes");
+			final ResultSet nodeSet = nodes.executeQuery();
+			vars.put("nodes", new IterableDataset() {
+				boolean first = true;
+
+				@Override
+				public boolean next(Map<String, Object> vars) {
+					try {
+						String sensname = null;
+						String sensdescr = null;
+						int receiver = 0;
+						if (first) {
+							first = false;
+							sensname = SensNetNodeConfiguration.getInstance()
+									.getNodeName() + " (this node)";
+							sensdescr = SensNetNodeConfiguration.getInstance()
+									.getNodeDescription();
+							receiver = SensNetNodeConfiguration.getInstance()
+									.getNodeID();
+						} else if (nodeSet.next()) {
+							sensname = nodeSet.getString("name");
+							sensdescr = nodeSet.getString("description");
+							receiver = nodeSet.getInt("uid");
+						} else {
+							return false;
+						}
+						vars.put("sensname", sensname);
+						vars.put("sensdescr", sensdescr);
+						PreparedStatement prep = DatabaseConnection
+								.getInstance()
+								.prepare(
+										"select * from datapoints a where not exists ( select * from datapoints b where b.`from` = a.`from` and a.`received` < b.`received` and b.`receivernode` = ?) and a.`receivernode` = ?");
+						prep.setInt(1, receiver);
+						prep.setInt(2, receiver);
+						final ResultSet res = prep.executeQuery();
+						vars.put("sensors", new IterableDataset() {
+
+							@Override
+							public boolean next(Map<String, Object> vars) {
+								try {
+									if (res.next()) {
+										vars.put(
+												"lastseen",
+												NodesOverviewPage.DATE_FORMAT.format(new Date(
+														res.getInt("received") * 1000)));
+										vars.put("sensorid", res.getInt("from"));
+										vars.put("sensorbattery",
+												res.getInt("battery") + " %");
+										String lat = res.getInt("locationlat")
+												+ "";
+										lat = NodesOverviewPage.ammendZero(lat,
+												8);
+										lat = lat.substring(0, lat.length() - 7)
+												+ "."
+												+ lat.substring(
+														lat.length() - 7,
+														lat.length());
+										vars.put("lat", lat);
+										String lng = res.getInt("locationlong")
+												+ "";
+										lng = NodesOverviewPage.ammendZero(lng,
+												8);
+										lng = lng.substring(0, lng.length() - 7)
+												+ "."
+												+ lng.substring(
+														lng.length() - 7,
+														lng.length());
+										vars.put("long", lng);
+										return true;
+									}
+								} catch (SQLException e) {
+									e.printStackTrace();
+								}
+								return false;
+							}
+
+						});
+						return true;
+					} catch (SQLException e) {
+						e.printStackTrace();
+					}
+					return false;
+				}
+			});
 			vars.put("sensdata", new IterableDataset() {
 				int i = 0;
 
