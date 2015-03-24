d79bdae61f225424d836240efca9e5baf72809e1
Janis Streib <janis@dogcraft.de>
2015-03-24 02:52:09 +0100

    assume a default template

--- src/net/sensnet/node/plugins/VisualizerPlugin.java
+++ src/net/sensnet/node/plugins/VisualizerPlugin.java
@@ -3,19 +3,28 @@ package net.sensnet.node.plugins;
 import net.sensnet.node.SensNetNodeConfiguration;
 import net.sensnet.node.pages.api.json.DatapointJSONApiPage;
 
-import com.sun.org.apache.xalan.internal.xsltc.compiler.Template;
+import org.cacert.gigi.output.template.Template;
 
 public abstract class VisualizerPlugin extends Plugin {
+	private Template defaultTemplate;
 
 	public VisualizerPlugin(SensNetNodeConfiguration configuration) {
 		super(configuration);
+		try {
+			defaultTemplate = new Template(getClass().getResource(
+					getClass().getSimpleName() + ".templ"));
+		} catch (Exception e) {
+			getLoger().info("No fitting template found.");
+		}
 	}
 
 	public abstract String getSensorName();
 
 	public abstract int getSensorType();
 
-	public abstract Template getTemplate();
+	public Template getTemplate() {
+		return defaultTemplate;
+	}
 
 	public abstract DatapointJSONApiPage getDatapointJSONApiPage();
 
