962ac22d5a99ab33ca5c5f9a1c08b3a8de537e91
Janis Streib <janis@dogcraft.de>
2015-04-02 14:38:39 +0200

    add: wiki page plugin

---
+++ plugins/net/sensnet/plugins/pages/foundation/SensNetWiki.java
@@ -0,0 +1,27 @@
+package net.sensnet.plugins.pages.foundation;
+
+import net.sensnet.node.Page;
+import net.sensnet.node.SensNetNodeConfiguration;
+import net.sensnet.node.pages.LinkPage;
+import net.sensnet.node.plugins.PagePlugin;
+
+public class SensNetWiki extends PagePlugin {
+	private LinkPage page;
+
+	public SensNetWiki(SensNetNodeConfiguration configuration) {
+		super(configuration);
+		page = new LinkPage("Wiki", "https://wiki.sensnet.net") {
+		};
+	}
+
+	@Override
+	public Page getPage() {
+		return page;
+	}
+
+	@Override
+	public String getPathName() {
+		return "wiki";
+	}
+
+}
