ae71c5437854e3cdbd94d02ddf10c0ee9194889b
Janis Streib <janis@dogcraft.de>
2015-04-23 02:55:02 +0200

    Java side OS X support

--- src/net/sensnet/node/natives/SerialPort.java
+++ src/net/sensnet/node/natives/SerialPort.java
@@ -3,15 +3,22 @@ package net.sensnet.node.natives;
 import java.io.File;
 
 public class SerialPort {
+	private static boolean loaded = false;
+
 	static {
 		if (System.getProperty("os.name").toLowerCase().contains("linux")) {
 			System.load(new File("native/linux/libserialport.so")
 					.getAbsolutePath());
+			loaded = true;
+		} else if (System.getProperty("os.name").toLowerCase().contains("mac")) {
+			System.load(new File("native/darwin/libserialport.so")
+					.getAbsolutePath());
+			loaded = true;
 		}
 	}
 
 	public static void setBoudRate(String port, int boud) {
-		if (System.getProperty("os.name").toLowerCase().contains("linux")) {
+		if (loaded) {
 			setBoudRate0(port, boud);
 		}
 	}
