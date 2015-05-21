304ad7504f604e1c273df15e6835f1776d7f4060
Janis Streib <janis@dogcraft.de>
2015-05-21 03:23:38 +0200

    UPD: possible cast content

--- plugins/net/sensnet/plugins/pages/foundation/CastPage.java
+++ plugins/net/sensnet/plugins/pages/foundation/CastPage.java
@@ -1,25 +1,30 @@
 package net.sensnet.plugins.pages.foundation;
 
 import java.io.IOException;
+import java.io.PrintWriter;
 import java.util.Map;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import net.sensnet.node.Page;
 import net.sensnet.node.SensNetNodeConfiguration;
+import net.sensnet.node.pages.NodesOverviewPage;
 import net.sensnet.node.plugins.PlainPagePlugin;
 
+import org.cacert.gigi.output.template.Outputable;
 import org.cacert.gigi.output.template.Template;
 
 public class CastPage extends PlainPagePlugin {
 	private static Template templ;
-
+	private Page nodePage;
 	static {
 		templ = new Template(CastPage.class.getResource("CastPage.templ"));
 	}
 
 	public CastPage(SensNetNodeConfiguration configuration) {
 		super(configuration);
+		nodePage = new NodesOverviewPage("ov");
 	}
 
 	@Override
@@ -28,8 +33,20 @@ public class CastPage extends PlainPagePlugin {
 	}
 
 	@Override
-	public void doGet(HttpServletRequest req, HttpServletResponse resp,
-			Map<String, Object> vars) throws IOException {
+	public void doGet(final HttpServletRequest req,
+			final HttpServletResponse resp, Map<String, Object> vars)
+			throws IOException {
+		vars.put("content", new Outputable() {
+
+			@Override
+			public void output(PrintWriter out, Map<String, Object> vars) {
+				try {
+					nodePage.doGet(req, resp, vars);
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+			}
+		});
 		templ.output(resp.getWriter(), vars);
 	}
 
