abe2d84ff6888ddd5b4e0d80aa9c7adbab2a96ba
Janis Streib <janis@dogcraft.de>
2016-06-23 15:33:38 +0200

    UPD: better plugin misconfigured error message

--- src/net/sensnet/node/plugins/Plugin.java
+++ src/net/sensnet/node/plugins/Plugin.java
@@ -6,25 +6,25 @@ import org.eclipse.jetty.util.log.Log;
 import org.eclipse.jetty.util.log.Logger;
 
 public abstract class Plugin {
-	private SensNetNodeConfiguration configuration;
+    private SensNetNodeConfiguration configuration;
 
-	public Plugin(SensNetNodeConfiguration configuration) {
-		this.configuration = configuration;
-	}
+    public Plugin(SensNetNodeConfiguration configuration) {
+        this.configuration = configuration;
+    }
 
-	public String getProperty(String property) {
-		String res = configuration.getProperty(getClass().getName()
-				.toLowerCase() + "." + property);
-		if (res == null) {
-			throw new IllegalArgumentException("Misconfigured plugin '"
-					+ getClass().getName()
-					+ "'! Couldn't find configuration property '" + property
-					+ "'.");
-		}
-		return res;
-	}
+    public String getProperty(String property) {
+        String desiredOrp = getClass().getName().toLowerCase() + "." + property;
+        String res = configuration.getProperty(desiredOrp);
+        if (res == null) {
+            throw new IllegalArgumentException("Misconfigured plugin '"
+                    + getClass().getName()
+                    + "'! Couldn't find configuration property '" + desiredOrp
+                    + "'.");
+        }
+        return res;
+    }
 
-	public Logger getLoger() {
-		return Log.getLogger("PLUGIN: " + getClass().getSimpleName());
-	}
+    public Logger getLoger() {
+        return Log.getLogger("PLUGIN: " + getClass().getSimpleName());
+    }
 }
